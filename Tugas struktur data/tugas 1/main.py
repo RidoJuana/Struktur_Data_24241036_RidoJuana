c95f46045c0813b4565d85acb57fc3016093c2eb
RidoJuana <zeexyee9@gmail.com>
2025-06-24 09:34:18 +0800

    Update main.py

--- Tugas struktur data/tugas 1/main.py
+++ Tugas struktur data/tugas 1/main.py
@@ -1,7 +1,7 @@
 # tugas 1
 
-baris = 3                        # Penjelasan:Menentukan jumlah baris segitiga. Karena total 9 bintang, dan kita tahu 1 + 3 + 5 = 9, maka cukup 3 baris.
-for i in range(baris): # ini adalah awal dari loop for, yang akan mengulang sebanyak baris kali (dalam hal ini, sebanyak 3 kali). Fungsi range(baris) menghasilkan urutan angka mulai dari 0 hingga baris-1. Jadi, i akan memiliki nilai
-    spasi = ' ' * (baris - i - 1) # spasi di gunakan agar bintang terlihat lebih simetris 
-    bintang = '*' * (2 * i + 1)   # menghitung jumlah bintang untuk tiap baris,selalu bilangan ganjil
-    print(spasi + bintang)        # mencetak hasil dari spasi + bintang
+baris = 3                        
+for i in range(baris): 
+    spasi = ' ' * (baris - i - 1) 
+    bintang = '*' * (2 * i + 1)   
+    print(spasi + bintang)        
